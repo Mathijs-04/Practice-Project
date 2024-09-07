d29768703bc2b2beceae25da63b31354e5a55b75
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-09-07 13:27:30 +0200

    Practice project edited

---
+++ evenmoresecret.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Wow you found it</title>
+</head>
+<body>
+<video controls height="900" width="1850">
+    <source src="img/VR%20DYSTOPIA.mp4" type="video/mp4">
+</video>
+</body>
+</html>
